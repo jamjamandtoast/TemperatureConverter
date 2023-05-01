8810bffd406a5a4360b37011baf662ce437fd738
Jennifer Roblero <jenniferroblero@Jennifers-MacBook-Pro.local>
2023-05-01 11:19:59 -0400

    edits

---
+++ scripts/fahrenheit.js
@@ -0,0 +1,9 @@
+"use sctrict"
+
+const input1 = document.getElementById('input-1');
+
+const input2 = document.getElementById('input-2');
+
+const convertBtn = document.getElementById('convertBtn');
+
+const input3 = document.getElementById('input-3');
